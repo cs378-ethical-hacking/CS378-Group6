4311882275a853b5dedf21a074c3108535543f34
root <root@localhost.localdomain>
2016-04-22 16:38:21 -0400

    pdf update

--- conver2PDF.py
+++ conver2PDF.py
@@ -1,7 +1,7 @@
 import pyPdf
 import sys
 import StringIO
-import Canvas
+from reportlab.pdfgen import canvas
 
 # make sure it doesn't access a random place
 assert len(sys.argv) >= 2
@@ -9,11 +9,23 @@ filename = sys.argv[1]
 
 txt = open(filename)
 packet = StringIO.StringIO()
-cv = Canvas.Canvas()
-cv.create_text(0, 500, text = txt)
-cv.pack()
+cv = canvas.Canvas(packet)
+depth = 0
+string = list()
+for line in txt:
+	line = line.replace ('\n', '')
+	string.append(line)
+
+y = 800
+cv.drawString(200, y, "scan report")
+y -= 50
+for line in string:
+	cv.drawString(20, y, line)
+	y -= 20
+	
+cv.save()
 packet.seek(0)
-with open("./scanOutput", "wb") as openfile:
-	openfile.write(packet.getValue())
+with open("./scanOutput.pdf", "wb") as openfile:
+	openfile.write(packet.getvalue())
 
 
