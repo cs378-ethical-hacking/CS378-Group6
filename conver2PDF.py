a6687c3d33093ce63533ec811107b145626cba98
root <xinyiwang@utexas.edu>
2016-04-26 23:48:07 -0400

    add font

--- conver2PDF.py
+++ conver2PDF.py
@@ -3,7 +3,8 @@ import sys
 import StringIO
 from reportlab.pdfgen import canvas
 from reportlab.rl_config import defaultPageSize
-
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 # make sure it doesn't access a random place
 assert len(sys.argv) >= 2
 filename = sys.argv[1]
@@ -20,9 +21,13 @@ for line in txt:
 	line = line.replace ('\n', '')
 	string.append(line)
 
-y = PAGE_HEIGHT - 10
+pdfmetrics.registerFont(TTFont('serif', 'LiberationSerif-Regular.ttf'))
+cv.setFont('serif', 30)
+y = PAGE_HEIGHT - 50
 cv.drawString(PAGE_WIDTH / 2, y, "scan report")
 y -= 50
+
+cv.setFont("serif", 20)
 for line in string:
 	cv.drawString(20, y, line)
 	y -= 20
