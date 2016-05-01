64fef48692fb53905dcba860640bcb53a52ade2e
tipparat umrod <tipparat.umrod@utexas.edu>
2016-05-01 16:46:03 -0500

    clean up repo

--- combine.py
+++ combine.py
@@ -12,48 +12,55 @@ from snmpwalk import *
 #### RUN NMAP SCAN ####
 filename = sys.argv[1]
 networkrange = sys.argv[2]
-
+#word_list = "/usr/share/john/"+ john_list
 def run_nmap():
-
+    print "nmap scan..."
     command = 'nmap -O -oA ' + filename + ' ' + networkrange
     print command
-    #coutput = commands.getstatusoutput(command)
 
     ## RUN NMAP COMMAND ##
     os.system(command)
 
-    ## PARSE THROUGH OUTPUT FILE ##
 
 def run_services(network_dict):
-    word_list = run_john("password.lst")
+    print "run services..."
+    word_list = run_john("./password.lst")
+
     for ip in network_dict:
+        print "host: " + ip
         services = network_dict[ip][1]
 
+        if "router" in network_dict[ip][3]:
+            print "run snmpwalk..."
+            run_snmpwalk(ip)
 
         for port in services:
             if port.split("/")[0] == '79':
+                print "run finger..."
                 finger_service = 'finger -l @' + ip
                 print finger_service
                 coutput = commands.getstatusoutput(finger_service)
-                # print type(coutput)
+
                 if("Timeout" not in coutput):
-                    #print "successful WOO!"
-                    write_to_file("port79.txt", coutput[1])
+                    print "successful--write to file--"
+                    write_to_file(ip+ "_output.txt", finger_service)
+                    write_to_file(ip+ "_output.txt", coutput[1])
 
             if port.split("/")[0] == '22':
+                print "run hydra..."
                 hydra = "hydra -l root -P /usr/share/john/password.lst ssh://"+ip
                 print hydra
                 coutput = commands.getstatusoutput(hydra)
-                #print coutput
+
                 if("[ERROR]" not in coutput[1]):
-                    #print "successful WOO!"
-                    write_to_file("port22.txt", coutput[1])
+                    print "successful--write to file--"
+                    write_to_file(ip+ "_output.txt", hydra)
+                    write_to_file(ip+ "_output.txt", coutput[1])
 
 
 
 def run_john(john_list):
-    #word_list = "/usr/share/john/"+ john_list
-    word_list = "./"+ john_list
+    word_list = john_list
     words = []
     with open(word_list, "r") as f:
         for line in f:
@@ -61,20 +68,19 @@ def run_john(john_list):
     return words
 
 def main():
-    #run_nmap()
-    #nmap_dict = set_dictionary(filename)
+    run_nmap()
+    nmap_dict = set_dictionary(filename)
 
-    run_snmpwalk('10.202.208.18')
-    #print nmap_dict
-    #run_services(nmap_dict)
+    run_services(nmap_dict)
 
     # Generate a CSV file corresponding to the NMAP scan output
-    #csv = create_csv()
+    csv = create_csv()
 
     # Write the CSV to an output file
-    #write_to_csv(filename, csv)
+    write_to_csv(filename, csv)
 
     # Use the generated CSV output file to export to HTML boostrap file
-    #csv_to_html(filename)
+    csv_to_html(filename)
 
-main()
+if __name__ == "__main__":
+    main()
--- snmpwalk.py
+++ snmpwalk.py
@@ -3,39 +3,30 @@ import commands
 
 def run_snmpwalk(ip):
     word_list = run_john("password.lst")
-    cmnity_str = "public"
     snmpwalk_public = "snmpwalk -v1 -c public " + ip
     print snmpwalk_public
     result_public = commands.getstatusoutput(snmpwalk_public)
 
-
-    if("Timeout" not in result_public[1]):
+    if('SNMP' in result_public[1]):
         print "successful--write to file--"
+        write_to_file(ip+"-snmpwalk.txt", snmpwalk_public)
         write_to_file(ip+"-snmpwalk.txt",result_public[1])
     else:
         for word in word_list:
             if "#" not in word:
                 snmpwalk_john = "snmpwalk -v1 -c " + word.rstrip() +" " + ip
+                print snmpwalk_john
                 coutput = commands.getstatusoutput(snmpwalk_john)
 
-                if('Timeout' not in coutput[1]):
-                    print "successful--save community string--"
-                    cmnity_str = word
-                    write_to_file(ip+"-snmpwalk.txt", result_public[1])
-                    break;
                 if('SNMP' in coutput[1]):
-                    print "successful--save community string--"
-                    print coutput[1]
-                    cmnity_str = word
+                    print "successful--write to file--"
+                    write_to_file(ip+"-snmpwalk.txt", snmpwalk_john)
                     write_to_file(ip+"-snmpwalk.txt", coutput[1])
                     break;
 
-
 def run_john(john_list):
-    #word_list = "/usr/share/john/"+ john_list
-    word_list = "./"+ john_list
     words = []
-    with open(word_list, "r") as f:
+    with open(john_list, "r") as f:
         for line in f:
             words.append(line)
     return words
